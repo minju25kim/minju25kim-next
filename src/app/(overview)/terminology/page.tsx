f78cdefb2d642af9ce7afa8c80e7b81c0e1ca775
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2025-01-20 14:43:42 +0900

    fix: add more wrapper

--- src/app/(overview)/terminology/page.tsx
+++ src/app/(overview)/terminology/page.tsx
@@ -1,15 +1,32 @@
 import { getAllPostsDirectory } from "@/lib/api";
 import Title from "@/components/AppComponents/PrimaryTitle";
 import AppTab from "@/components/AppComponents/Tab";
-
+import { Post } from "@/interfaces/Data";
 
 export default async function Page() {
-  const allPosts = await getAllPostsDirectory("terminology");
+  let allPosts: Post[] = []
+  async function fetchAllPosts() {
+    try {
+      const allPosts = await getAllPostsDirectory("terminology");
+      return allPosts;
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+      // Handle the error as needed, for example, return an empty array or null
+      return [];
+    }
+  }
+
+  fetchAllPosts().then(posts => {
+    console.log(posts);
+    allPosts = posts
+  }).catch(error => {
+    console.error("Unexpected error:", error);
+  });
 
   return (
     <>
       <Title title="Terminology" />
-      <AppTab  views={["table","card"]} allPosts={allPosts} />
+      <AppTab views={["table", "card"]} allPosts={allPosts} />
     </>
   );
 }
