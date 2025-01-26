bdb39b3cc38075da5897a5d0192d63710a0526f8
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2025-01-26 17:38:26 +0900

    fix: change app/page like resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Title from "@/components/AppComponents/PrimaryTitle";
 import Link from "next/link";
 import { GitHubIcon, LinkedInIcon, TwitterIcon } from "@/components/icons";
@@ -7,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import Card from "@/components/AppComponents/Card";
 import { Content } from '@/interfaces/Data';
 import Badge from "@/components/AppComponents/Badge";
+import { useEffect, useState } from "react";
 
 const links = [
   { title: 'github', url: 'https://github.com/minju25kim', icon: GitHubIcon },
@@ -15,31 +17,25 @@ const links = [
   { title: 'email', url: 'mailto:minju25kim@gmail.com', icon: MailIcon },
 ];
 
-const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://minju25kim.fly.dev/api/content'
-  : 'http://localhost:3001/api/content';
+export default function Page() {
+  const [results, setResults] = useState<Content[]>([]);
 
-async function fetchData() {
-  try {
-    const response = await fetch(API_URL, {
-      method: 'GET'
-    });
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/content', {
+          method: 'GET'
+        });
+        const data = await response.json();
+        setResults(data)
+      } catch (error) {
+        console.error("Error fetching contents:", error);
+      }
+    };
 
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return [];
-  }
-}
-
-export default async function Page() {
-  const contents = await fetchData();
-
+    fetchData();
+  }, []);
+  
   return (
     <>
       <div className="flex flex-col items-center md:items-start mb-4">
@@ -74,7 +70,7 @@ export default async function Page() {
       <Badge />
       <div className="flex flex-col mx-auto w-full mt-2">
         <SecondaryTitle title="Latest contents" />
-        <Card allContent={contents} />
+        <Card allContent={results} />
       </div>
     </>
   );
