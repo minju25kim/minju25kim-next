ed7a769694481eb6a740a2d60bf11e94fab48eec
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2025-01-29 11:27:25 +0900

    fix: temporary commnet out

--- src/app/api/view/route.ts
+++ src/app/api/view/route.ts
@@ -1,35 +1,35 @@
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-export async function POST(req: NextRequest) {
-    const contentId = req.nextUrl.searchParams.get('contentId');
-    if (!contentId) {
-        return NextResponse.json({ error: 'contentId is required' }, { status: 400 });
-    }
-    // console.log(contentId)
+// export async function POST(req: NextRequest) {
+//     const contentId = req.nextUrl.searchParams.get('contentId');
+//     if (!contentId) {
+//         return NextResponse.json({ error: 'contentId is required' }, { status: 400 });
+//     }
+//     // console.log(contentId)
 
-    try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/content/${contentId}/view`, {
-            method: 'POST',
-        });
-        const data = await response.json();
-        // console.log(data)
-        return NextResponse.json(data, { status: 200 });
-    } catch (error) {
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-    }
-}
+//     try {
+//         const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/content/${contentId}/view`, {
+//             method: 'POST',
+//         });
+//         const data = await response.json();
+//         // console.log(data)
+//         return NextResponse.json(data, { status: 200 });
+//     } catch (error) {
+//         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+//     }
+// }
 
-export async function GET(req: NextRequest) {
-    const contentId = req.nextUrl.searchParams.get('contentId');
-    if (!contentId) {
-        return NextResponse.json({ error: 'contentId is required' }, { status: 400 });
-    }
-    try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/content/${contentId}/views`);
-        const data = await response.json();
-        // console.log(data)
-        return NextResponse.json(data, { status: 200 });
-    } catch (error) {
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-    }
-}          
+// export async function GET(req: NextRequest) {
+//     const contentId = req.nextUrl.searchParams.get('contentId');
+//     if (!contentId) {
+//         return NextResponse.json({ error: 'contentId is required' }, { status: 400 });
+//     }
+//     try {
+//         const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/content/${contentId}/views`);
+//         const data = await response.json();
+//         // console.log(data)
+//         return NextResponse.json(data, { status: 200 });
+//     } catch (error) {
+//         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+//     }
+// }          
