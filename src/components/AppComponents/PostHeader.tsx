a8eeb51c8d35691f2ca246c6cb39cf2c5c7d40b5
Minju Kim <48757517+minju25kim@users.noreply.github.com>
2025-01-29 11:25:28 +0900

    fix: temporary comment out view comps

--- src/components/AppComponents/PostHeader.tsx
+++ src/components/AppComponents/PostHeader.tsx
@@ -1,5 +1,5 @@
 import PostTitle from '@/components/AppComponents/SecondaryTitle';
-// import { dateString } from "@/lib/utils";
+import { dateString } from "@/lib/utils";
 import Keywords from '@/components/AppComponents/Keywords'
 // import Views from '@/components/AppComponents/Views'
 type Props = {
@@ -11,11 +11,13 @@ type Props = {
     contentId: string
 };
 
-export function PostHeader({ title, date, author, keywords, contentId }: Props) {
+export function PostHeader({ title, date, author, keywords }: Props) {
     return (
         <div className="mb-4">
             <PostTitle title={title} />
-            {/* <div className="text-center md:text-justify text-sm text-muted-foreground">{author} | {dateString(date)} | <Views contentId={contentId} incrementOnMount={true} /> views</div> */}
+            <div className="text-center md:text-justify text-sm text-muted-foreground">{author} | {dateString(date)} |
+                {/* <Views contentId={contentId} incrementOnMount={true} /> */}
+                views</div>
             <Keywords keywords={keywords} />
         </div>
     )
